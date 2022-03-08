dd060a2ef7482c389fe0d33ae77a1cb9a9e4b6f7
zebingzhong <zhongzebing@cjdgy.com>
2022-03-08 17:57:42 +0800

    init

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 			"challenge": rJson.Challenge,
 		})
 	})
-	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func Decrypt(encrypt string, key string) (string, error) {
